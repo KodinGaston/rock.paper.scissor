ce7d49554b87c93ee885065f660553aa07e1d6d2
kodingaston <gaston.sanchez@medieinstitutet.se>
2025-03-27 15:06:14 +0100

    Change: vite config so hidden pics can be seen.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/rock.paper.scissor/", 
+
   server: {
     host: true,  // Permite acceder desde LAN
     https: false, // Asegura que no fuerce HTTPS en local
   },
-  base: '/rock.paper.scissor/',
+  
   build: {
     outDir: "dist",
   },
